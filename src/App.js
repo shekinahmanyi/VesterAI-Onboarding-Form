36ecb0a30676ffb505e2d032e405a30e3b47cb39
shekinahmanyi <shekinahmanyi@gmail.com>
2024-01-31 10:52:11 +0100

    Added the various routes in the app file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from "react";
 import TellUsForm from "./pages/TellUsForm";
+import StartupForm from "./pages/StartupForm";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     <TellUsForm/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<TellUsForm />} />
+        <Route path="/step2" element={<StartupForm />} />
+      </Routes>
+    </Router>
   );
 }
 
